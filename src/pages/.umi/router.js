46488baf2d2bc6152c65ec9f709e3de4607c5892
zhuhuaqiang <616928019@qq.com>
2018-07-24 11:10:45 +0800

    merge
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

--- src/pages/.umi/router.js
+++ src/pages/.umi/router.js
@@ -17,24 +17,9 @@ let routes = [
     "component": require('../../layouts/index.js').default,
     "routes": [
       {
-        "path": "/admin",
-        "exact": true,
-        "component": require('../admin.js').default
-      },
-      {
-        "path": "/delay",
-        "exact": true,
-        "component": require('../delay.js').default
-      },
-      {
         "path": "/",
         "exact": true,
         "component": require('../index/index.js').default
-      },
-      {
-        "path": "/login",
-        "exact": true,
-        "component": require('../login.js').default
       },
       {
         "path": "/list",
@@ -57,12 +42,12 @@ let routes = [
             "component": require('../list/search/index.js').default
           },
           {
-            "component": () => React.createElement(require('C:/Users/Administrator/Desktop/webGL/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', routes: '[{"path":"/","component":"./src\\\\layouts\\\\index.js","routes":[{"path":"/admin","exact":true,"component":"./src/pages/admin.js"},{"path":"/delay","exact":true,"component":"./src/pages/delay.js"},{"path":"/index/components/Count","exact":true,"component":"./src/pages/index/components/Count.js"},{"path":"/","exact":true,"component":"./src/pages/index/index.js"},{"path":"/index/model","exact":true,"component":"./src/pages/index/model.js"},{"path":"/login","exact":true,"component":"./src/pages/login.js"},{"path":"/list","exact":false,"component":"./src/pages/list/_layout.js","routes":[{"path":"/list","exact":true,"component":"./src/pages/list/index.js"},{"path":"/list/list","exact":true,"component":"./src/pages/list/list.js"},{"path":"/list/models/a","exact":true,"component":"./src/pages/list/models/a.js"},{"path":"/list/models/b","exact":true,"component":"./src/pages/list/models/b.js"},{"path":"/list/search","exact":true,"component":"./src/pages/list/search/index.js"},{"path":"/list/search/model","exact":true,"component":"./src/pages/list/search/model.js"}]}]}]' })
+            "component": () => React.createElement(require('C:/Users/Administrator/Desktop/webGL/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', routes: '[{"path":"/","component":"./src\\\\layouts\\\\index.js","routes":[{"path":"/","exact":true,"component":"./src/pages/index/index.js"},{"path":"/index/model","exact":true,"component":"./src/pages/index/model.js"},{"path":"/list","exact":false,"component":"./src/pages/list/_layout.js","routes":[{"path":"/list","exact":true,"component":"./src/pages/list/index.js"},{"path":"/list/list","exact":true,"component":"./src/pages/list/list.js"},{"path":"/list/models/a","exact":true,"component":"./src/pages/list/models/a.js"},{"path":"/list/models/b","exact":true,"component":"./src/pages/list/models/b.js"},{"path":"/list/search","exact":true,"component":"./src/pages/list/search/index.js"},{"path":"/list/search/model","exact":true,"component":"./src/pages/list/search/model.js"}]}]}]' })
           }
         ]
       },
       {
-        "component": () => React.createElement(require('C:/Users/Administrator/Desktop/webGL/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', routes: '[{"path":"/","component":"./src\\\\layouts\\\\index.js","routes":[{"path":"/admin","exact":true,"component":"./src/pages/admin.js"},{"path":"/delay","exact":true,"component":"./src/pages/delay.js"},{"path":"/index/components/Count","exact":true,"component":"./src/pages/index/components/Count.js"},{"path":"/","exact":true,"component":"./src/pages/index/index.js"},{"path":"/index/model","exact":true,"component":"./src/pages/index/model.js"},{"path":"/login","exact":true,"component":"./src/pages/login.js"},{"path":"/list","exact":false,"component":"./src/pages/list/_layout.js","routes":[{"path":"/list","exact":true,"component":"./src/pages/list/index.js"},{"path":"/list/list","exact":true,"component":"./src/pages/list/list.js"},{"path":"/list/models/a","exact":true,"component":"./src/pages/list/models/a.js"},{"path":"/list/models/b","exact":true,"component":"./src/pages/list/models/b.js"},{"path":"/list/search","exact":true,"component":"./src/pages/list/search/index.js"},{"path":"/list/search/model","exact":true,"component":"./src/pages/list/search/model.js"}]}]}]' })
+        "component": () => React.createElement(require('C:/Users/Administrator/Desktop/webGL/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', routes: '[{"path":"/","component":"./src\\\\layouts\\\\index.js","routes":[{"path":"/","exact":true,"component":"./src/pages/index/index.js"},{"path":"/index/model","exact":true,"component":"./src/pages/index/model.js"},{"path":"/list","exact":false,"component":"./src/pages/list/_layout.js","routes":[{"path":"/list","exact":true,"component":"./src/pages/list/index.js"},{"path":"/list/list","exact":true,"component":"./src/pages/list/list.js"},{"path":"/list/models/a","exact":true,"component":"./src/pages/list/models/a.js"},{"path":"/list/models/b","exact":true,"component":"./src/pages/list/models/b.js"},{"path":"/list/search","exact":true,"component":"./src/pages/list/search/index.js"},{"path":"/list/search/model","exact":true,"component":"./src/pages/list/search/model.js"}]}]}]' })
       }
     ]
   }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,9 @@
 import { connect } from 'dva';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer } from 'react-leaflet'
 import L from 'leaflet';
 import 'proj4';
 import 'proj4leaflet';
+import 'leaflet/dist/leaflet.css'
 import styles from './index.css';
 import React, { PureComponent } from 'react';
 
@@ -13,7 +14,7 @@ class App extends PureComponent {
     zoom: 13,
   }
   render() {
-    const {center,zoom} = this.state
+    const { center, zoom } = this.state
     // 百度地图专属设置,请引入 proj4.js 和 proj4leaflet.js
     const bdCrs = new L.Proj.CRS('EPSG:900913', '+proj=merc +a=6378206 +b=6356584.314245179 +lat_ts=0.0 +lon_0=0.0 +x_0=0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', {
       resolutions: function () {
@@ -30,20 +31,22 @@ class App extends PureComponent {
     });
 
     return (
-      <Map
-        crs={bdCrs}
-        center={center}
-        zoom={zoom}
-        attributionControl={false}
-        zoomControl={false}
-        className={styles.root}
-      >
-        <TileLayer
-          subdomains={[0, 1, 2]}
-          tms
-          url="http://online{s}.map.bdimg.com/tile/?qt=tile&x={x}&y={y}&z={z}&styles=pl&udt=20180201"
-        />
-      </Map>
+
+        <Map
+          crs={bdCrs}
+          center={center}
+          zoom={zoom}
+          attributionControl={false}
+          zoomControl={false}
+          className={styles.root}
+        >
+          <TileLayer
+            subdomains={[0, 1, 2]}
+            tms
+            url="http://online{s}.map.bdimg.com/tile/?qt=tile&x={x}&y={y}&z={z}&styles=pl&udt=20180201"
+          />
+        </Map>
+
     );
   }
 }
